ab4ac9f5ae1aa70c28e99a2b5c2a470b9ef48af8
Rezaul <mdrezaulkarrim@gmail.com>
2024-11-06 21:29:21 +0600

    completed task 5

---
+++ src/practise-4.ts
@@ -0,0 +1,32 @@
+interface Circle {
+  shape: 'circle';
+  radius: number;
+}
+
+interface Rectangle {
+  shape: 'rectangle';
+  width: number;
+  height: number;
+}
+
+type Area = Circle | Rectangle;
+
+const calculateShapeArea = (obj: Area): number => {
+  if (obj.shape === 'circle') return Math.PI * obj.radius * obj.radius;
+  else {
+    return obj.width * obj.height;
+  }
+};
+
+const rectangleArea = calculateShapeArea({
+  shape: 'rectangle',
+  width: 4,
+  height: 6,
+});
+
+const circleArea = parseInt(
+  calculateShapeArea({ shape: 'circle', radius: 5 }).toFixed(2)
+);
+console.log(circleArea);
+
+console.log(rectangleArea);
