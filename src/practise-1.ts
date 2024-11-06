313ec4b5c9e3db24b5864a52b804647008f8f1b2
Rezaul <mdrezaulkarrim@gmail.com>
2024-11-06 20:24:27 +0600

    completed task 1

--- src/practise-1.ts
+++ src/practise-1.ts
@@ -1,1 +1,10 @@
-console.log('Hello Typescript');
+interface SumArr {
+  (arr: number[]): number;
+}
+
+const sumArray: SumArr = (arr) => {
+  return arr.reduce((acc: number, cur: number): number => acc + cur, 0);
+};
+
+const val = sumArray([1, 2, 3, 4]);
+console.log(val);
