c76d4e960dce1067e7a340f8e67d55135b893203
Rezaul <mdrezaulkarrim@gmail.com>
2024-11-06 20:33:30 +0600

    completed task 2

---
+++ src/practise-2.ts
@@ -0,0 +1,9 @@
+interface RemoveDuplicateArr {
+  (arr: number[]): number[];
+}
+
+const removeDuplicates: RemoveDuplicateArr = (arr) => [...new Set(arr)];
+
+const uniqueArrVal = removeDuplicates([1, 2, 2, 3, 4, 4, 5]);
+
+console.log(uniqueArrVal);
